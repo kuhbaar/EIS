eb5dbace6440649ddb7e2e51630b25f3e74e1142
Daniel <Daniel>
2014-05-21 14:04:02 +0200

    add von CPPIPortfolio vergessen

---
+++ PDCA/src/at/ac/tuwien/imw/pdca/cppi/CPPIPortfolio.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.imw.pdca.cppi;
+
+import java.math.BigDecimal;
+
+import at.ac.tuwien.imw.pdca.MeasuredPerformanceValue;
+
+public class CPPIPortfolio extends MeasuredPerformanceValue<BigDecimal> {
+	
+	public CPPIPortfolio(BigDecimal value) {
+		super(value);
+	}
+
+}
